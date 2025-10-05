b519a5e01a8454855ea483e8823e13448bca5851
German <gerez777@gmail.com>
2025-10-05 12:11:37 -0300

    Proyecto completo frontend y backend

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Configuración de CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/contact', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'germandovich@gmail.com',
+      pass: 'ylom tbzu bmif exet'
+    }
+  });
+
+  let mailOptions = {
+    from: email,
+    to: 'germandovich@gmail.com',
+    subject: 'Nuevo mensaje de contacto',
+    text: `Nombre: ${name}\nEmail: ${email}\nMensaje:\n${message}`
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.send('Mensaje enviado');
+  } catch (error) {
+    console.error('Error al enviar el mensaje:', error);
+    res.status(500).send('Error al enviar el mensaje');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en puerto 3000');
+});
